b4c10af4be7e4cdd668f9f85d0a425f4ab81f43e
palokap <simbdigger@gmail.com>
2023-01-09 14:04:43 +0300

    more fixes commit

--- scripts/wait-script.sh
+++ scripts/wait-script.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
-IFS="," read -ra PORTS <<<"$WAIT_PORTS"
+echo "hello"
 
+IFS="," read -ra PORTS <<<"$WAIT_PORTS"
+path=$(dirname "$0")
+
+echo "hello2"
 PIDs=()
 for port in "${PORTS[@]}"; do
-  ./scripts/wait-for.sh -t 10 "localhost:$port" -- echo "Host localhost:$port is active" &
+    echo "for1"
+  "$path"/wait-for.sh -t 120 "http://localhost:$port/manage/health" -- echo "Host localhost:$port is active" &
   PIDs+=($!)
 done
 
+echo "hello3"
 for pid in "${PIDs[@]}"; do
   if ! wait "${pid}"; then
     exit 1
   fi
 done
+echo "hello4"
